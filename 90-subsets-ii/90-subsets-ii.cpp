6810e9260f3dc990dd2007f3bc7e1d8bb8e8cd13
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2022-09-16 03:12:35 +0530

    Time: 8 ms (41.27%), Space: 8.1 MB (40.71%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void sub(int ind,vector<int> nums,vector<int>temp,set<vector<int>> &ans)
+    void sub(int ind,vector<int> nums,vector<int>temp,vector<vector<int>> &ans)
     {
-        ans.insert(temp);
+        ans.push_back(temp);
        for(int i=ind;i<nums.size();i++)
        {
+           if(i>ind && nums[i]==nums[i-1])continue;
           temp.push_back(nums[i]);
            sub(i+1,nums,temp,ans);
             temp.pop_back();
        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> ans1;
+        // set<vector<int>> ans1;
          vector<vector<int>> ans;
         vector<int> temp;
         sort(nums.begin(),nums.end());
-        sub(0,nums,temp,ans1);
-        for(auto i:ans1)
-        {
-        ans.push_back(i);}
+        sub(0,nums,temp,ans);
+        // for(auto i:ans1)
+        // {
+        // ans.push_back(i);}
         return ans;
     }
 };

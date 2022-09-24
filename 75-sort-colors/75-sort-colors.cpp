f0a896dc958f846c88bf7f929020f9166147fd03
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2022-09-24 12:08:39 +0530

    Time: 7 ms (17.89%), Space: 8.4 MB (23.53%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,12 @@
-class Solution {
+class Solution { 
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int i = 0, j = i, k = nums.size() - 1;
+        
+        while(j <= k){
+            if(nums[j] == 0) swap(nums[i++], nums[j++]);
+            else if(nums[j] == 1) j++;
+            else swap(nums[k--], nums[j]);
+        }
     }
-};
+}; 

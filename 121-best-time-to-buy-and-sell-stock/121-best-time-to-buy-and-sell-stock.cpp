1b96a55e787e25dc30094cb2602c62b7ff66192c
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2022-10-01 00:49:50 +0530

    Time: 110 ms (99.75%), Space: 93.4 MB (10.68%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxp=0,minp=1e6;
+        int maxp=0,minp=1e5;
         for(auto i :prices)
         {
             minp=min(minp,i);

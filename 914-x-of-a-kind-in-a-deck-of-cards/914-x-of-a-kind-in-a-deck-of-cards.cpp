16e3f70f72fc0a0e643d028ac65a23ab84766c00
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2022-10-06 03:30:24 +0530

    Time: 45 ms (7.41%), Space: 17.5 MB (19.95%) - LeetHub

--- 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
     bool hasGroupsSizeX(vector<int>& deck) {
-        map<int,int>mp;
-        int m=INT_MAX;
-        for(auto i:deck)
-        {
-            mp[i]++;
-        }
-        for(auto i:mp)
-        {
-            if(i.second<2)
-                return false;
-            m=min(m,i.second);
-           
-        }bool flg ;   while(m>=2){flg = true;
-           for(auto a : mp){
-                if(a.second % m!= 0){
-                    flg = false;
-                    break;
-                }
-            }if(flg)return flg;
-                                  m--;
-                       }
-            
-            
-                return false;
+        map<int, int> map;
+        int ans = 0;
+        for (auto i : deck) map[i]++;
+        for (auto i : map) ans = __gcd(i.second, ans);
+        return ans > 1;
     }
 };

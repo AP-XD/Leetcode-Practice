22f9cb2d6854c7abdf551fe01a3e0eda1c2eb223
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2022-10-05 15:50:18 +0530

    Time: 7 ms (20.90%), Space: 8.4 MB (36.78%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int>n2=nums;
-        for(auto i:n2)
-        {
-            nums.push_back(i);
-        }
-        int mxc=0,maxc=0;
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            if(nums[i]<=nums[i+1])
-                mxc++;
-            else
-            {mxc=0;}
-            maxc=max(maxc,mxc);
-        }
-        // cout<<maxc;
-        if(maxc>=n2.size()-1)return true;
-        else
-            return false;
+        int c=0;
+       for(int i=0;i<nums.size();i++)
+       {
+           if(nums[i]>nums[(i+1)%(nums.size())])
+               c++;
+       }
+        return c<=1;
     }
 };

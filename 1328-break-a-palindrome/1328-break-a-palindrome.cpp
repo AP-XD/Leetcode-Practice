977f2c0bf4797e3edd5b7a92ad98a08c22c5782f
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2022-10-10 12:43:00 +0530

    Time: 3 ms (45.61%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -13,8 +13,7 @@ class Solution
                 for (int i = 0; i < p.length(); i++) {
                     if(p[i]!='a')
                     {p[i]='a';break;}
-                    else if(i==p.length()-1 && p[i]=='a')
-                        p[i]='b';
+                   
                 }
                 string cp=p;
                 reverse(p.begin(),p.end());

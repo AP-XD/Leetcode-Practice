dbdf1a407049e47e406b364a43cfcfa151d8bf9c
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2022-12-23 18:26:48 +0530

    Time: 15 ms (72.36%), Space: 7.5 MB (13.53%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>mp1,mp2;
+        for(auto i:s)
+        {
+            mp1[i]++;
+        }
+        for(auto i:t)
+        {
+            mp2[i]++;
+        }
+        if(mp1==mp2)return true;
+        else
+            return false;
+    }
+};
